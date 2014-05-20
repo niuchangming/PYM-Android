c8fc26f8952747a9c0f2f70123a08b5b740e2634
chancy <chancy3100@gmail.com>
2014-05-20 15:39:39 +0800

    finish posting the data to server

--- Brandsfever/src/com/brandsfever/SupportActivity.java
+++ Brandsfever/src/com/brandsfever/SupportActivity.java
@@ -1,29 +1,52 @@
 package com.brandsfever;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.protocol.HTTP;
+import org.json.JSONObject;
+
 import android.app.Fragment;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnCancelListener;
 import android.graphics.Typeface;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.text.TextUtils;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.navdrawer.SimpleSideDrawer;
+import com.progressbar.ProgressHUD;
+import com.ssl.HttpsClient;
+import com.ssl.TrustAllCertificates;
+
 
 public class SupportActivity extends FragmentActivity {
 
 	private static final String TAG = "SupportActivity";
 	
-	private String mOrderId;
-	private String mIssue;
-	private String mDetails;
-	private String mEmail;
-	
+	SimpleSideDrawer slide_me;
 	Button mAll, mMen, mWomen, mChildren, mHome, mAccessories, mLogin, mSettings, mCart, mInvite, mLogout;
-	Typeface mFont;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -37,18 +60,32 @@ public class SupportActivity extends FragmentActivity {
 					.add(R.id.container, new SupportFragment()).commit();
 		}
 		
+		mFont = Typeface.createFromAsset(getAssets(), "fonts/georgia.ttf");
+	
 	}
 
 	/**
 	 * A placeholder fragment containing a simple view.
 	 */
-	public static class SupportFragment extends Fragment {
+	public class SupportFragment extends Fragment {
 
 		Button mSubmit;
 		Typeface mFont;
+		
+		EditText mOrderId;
+		EditText mIssue;
+		EditText mDetail;
+		EditText mEmail;
+		
 		public SupportFragment() {
 		}
 
+		@Override
+		public void onCreate(Bundle savedInstanceState) {
+			// TODO Auto-generated method stub
+			super.onCreate(savedInstanceState);
+		}
+		
 		@Override
 		public View onCreateView(LayoutInflater inflater, ViewGroup container,
 				Bundle savedInstanceState) {
@@ -60,6 +97,10 @@ public class SupportActivity extends FragmentActivity {
 			TextView title = (TextView) rootView.findViewById(R.id.support_title);
 			title.setTypeface(mFont,Typeface.ITALIC);
 			
+			mOrderId = (EditText) rootView.findViewById(R.id.order_id);
+			mIssue = (EditText) rootView.findViewById(R.id.issue);
+			mDetail = (EditText) rootView.findViewById(R.id.detail);
+			mEmail = (EditText) rootView.findViewById(R.id.email);
 			
 			mSubmit = (Button)rootView.findViewById(R.id.support_submit);
 			mSubmit.setTypeface(mFont);
@@ -69,6 +110,26 @@ public class SupportActivity extends FragmentActivity {
 				public void onClick(View v) {
 					// TODO Auto-generated method stub
 					Log.d(TAG, "Submit is clicked.");
+					
+					if (mOrderId.getText().toString().length() < 1){
+						showMessage("Please input your Order Identifier.");
+					}
+					else if (mIssue.getText().toString().length() < 1){
+						showMessage("Please input your issue.");
+					}
+					else if (!isValidEmail(mEmail.getText().toString())) {
+						showMessage("Please input a valid Email address.");
+					}
+					else {
+						List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(4);
+						nameValuePairs.add(new BasicNameValuePair("order_number", mOrderId.getText().toString()));
+						nameValuePairs.add(new BasicNameValuePair("issue", mIssue.getText().toString()));
+						nameValuePairs.add(new BasicNameValuePair("detail",mDetail.getText().toString()));
+						nameValuePairs.add(new BasicNameValuePair("email", mEmail.getText().toString()));
+						
+						SubmitSupport submit = new SubmitSupport();
+						submit.execute(nameValuePairs);
+					}
 //					getActivity().getFragmentManager().beginTransaction().remove(SupportFragment.this).commit();
 				}
 			});
@@ -76,5 +137,97 @@ public class SupportActivity extends FragmentActivity {
 			return rootView;
 		}
 	}
+	
+	class SubmitSupport extends AsyncTask<List<NameValuePair>, String, String> implements OnCancelListener {
 
+		private ProgressHUD mProgressHUD;
+		
+		@Override
+		protected void onPreExecute() {
+			// TODO Auto-generated method stub
+			super.onPreExecute();
+			mProgressHUD = ProgressHUD.show(SupportActivity.this, "Loading", true, true, this);
+			
+		}
+		
+		@Override
+		public void onCancel(DialogInterface dialog) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		protected String doInBackground(List<NameValuePair>... params) {
+			String supportUrl = "https://www.brandsfever.com/api/v5/customer-support/";
+			
+			String result = postData(supportUrl,params[0]);
+			return result;
+		}
+		
+		@Override
+		protected void onPostExecute(String result) {
+			// TODO Auto-generated method stub
+			super.onPostExecute(result);
+			mProgressHUD.dismiss();
+			showMessage(result);
+		}
+		
+	}
+	
+	private String postData(String url, List<NameValuePair> nameValuePairs){
+		String response = null;
+		
+		TrustAllCertificates cert = new TrustAllCertificates();
+		cert.trustAllHosts();
+		HttpClient httpClient = HttpsClient.getNewHttpClient();
+		HttpPost httpPost = new HttpPost(url);
+		try {
+			httpPost.setEntity(new UrlEncodedFormEntity(nameValuePairs, HTTP.UTF_8));
+			HttpResponse httpResponse = httpClient.execute(httpPost);
+			int responseCode = httpResponse.getStatusLine().getStatusCode();
+			if(responseCode == 200){
+				InputStream inputStream = httpResponse.getEntity().getContent();
+				BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+				StringBuilder total = new StringBuilder();
+				String line;
+				while((line = reader.readLine()) != null){
+					total.append(line);
+				}
+				
+				JSONObject responseObject = new JSONObject(total.toString());
+				String responseMsg = responseObject.getString("msg");
+				if(responseMsg.equalsIgnoreCase("OK")){
+					response = "Thanks for your feedback. Happy shopping with Brandsfever:)";
+				}
+				else {
+					response = "Your Order Identifier is invalid.Please check and try again.";
+				}
+			} 
+			else {
+				response = "Please try again later.";
+			}
+		}
+		catch (Exception e){
+			e.printStackTrace();
+		}
+		
+		
+		return response;
+	}
+
+	
+	private void showMessage(String message) {
+		View view = View.inflate(SupportActivity.this.getBaseContext(),
+				R.layout.error_popop, null);
+		TextView msg = (TextView) view.findViewById(R.id._seterrormsg);
+		msg.setText(message);
+		Toast toast = new Toast(SupportActivity.this.getBaseContext());
+		toast.setGravity(Gravity.CENTER, 0, 0);
+		toast.setView(view);
+		toast.show();
+	}
+	
+	public final static boolean isValidEmail(CharSequence target){		
+		return !TextUtils.isEmpty(target)&& android.util.Patterns.EMAIL_ADDRESS.matcher(target).matches();
+	}
 }
