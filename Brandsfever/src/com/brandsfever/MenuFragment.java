93e4a173d9e6b7af128a29f1001bff7a142d1e9d
chancy <chancy3100@gmail.com>
2014-07-30 17:19:03 +0800

    Add implemention for onListItemClick for CampaignList

--- Brandsfever/src/com/brandsfever/MenuFragment.java
+++ Brandsfever/src/com/brandsfever/MenuFragment.java
@@ -12,25 +12,35 @@ import android.widget.ListView;
 
 public class MenuFragment extends ListFragment {
 	private static final String TAG = "MenuFragment";
+	private String[] mMenus;
+	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
+
+		mMenus = getResources().getStringArray(R.array.menus);
 		return inflater.inflate(R.layout.menu_list, null);
 	}
 	
 	@Override
 	public void onActivityCreated(Bundle savedInstanceState) {
 		super.onActivityCreated(savedInstanceState);
-		String[] menus = getResources().getStringArray(R.array.menus);
 		ArrayAdapter<String> menuAdapter = new ArrayAdapter<String>(getActivity(),
-				android.R.layout.simple_list_item_1,android.R.id.text1, menus);
+				android.R.layout.simple_list_item_1,android.R.id.text1, mMenus);
 		setListAdapter(menuAdapter);
 	}
 	
 	@Override
 	public void onListItemClick(ListView l, View v, int position, long id) {
 		super.onListItemClick(l, v, position, id);
-		Log.i(TAG, "Click on " + position);
+
+		if(position < mMenus.length){ // The category is clicked.
+			if(getActivity() instanceof ProductDisplay){
+				((ProductDisplay)getActivity()).toggle();
+			}
+			
+			Log.i(TAG, "Click on " + position);
+		}
 	}
 	
 	private void switchFragment(Fragment fragment) {
