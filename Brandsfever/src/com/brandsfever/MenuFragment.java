d5d7e05344300b365cec90fd230f79cc28f009d1
chancy <chancy3100@gmail.com>
2014-08-04 18:53:01 +0800

    Add header to sliding menu

--- Brandsfever/src/com/brandsfever/MenuFragment.java
+++ Brandsfever/src/com/brandsfever/MenuFragment.java
@@ -11,13 +11,17 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.TextView;
 
 public class MenuFragment extends ListFragment {
 	private static final String TAG = "MenuFragment";
 	private String[] mMenus;
 	private List<String> mCategories;
 
+	public TextView mHeaderTextView;
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -29,14 +33,20 @@ public class MenuFragment extends ListFragment {
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		return inflater.inflate(R.layout.menu_list, null);
+		ListView view = (ListView)inflater.inflate(R.layout.menu_list, null);
+		LinearLayout headerView = (LinearLayout)inflater.inflate(R.layout.menu_list_header, null);
+		mHeaderTextView = (TextView)headerView.findViewById(R.id.menu_header);
+		mHeaderTextView.setText("Hi! Changxi");
+		view.addHeaderView(headerView);
+		
+		return view;
 	}
 
 	@Override
 	public void onActivityCreated(Bundle savedInstanceState) {
 		super.onActivityCreated(savedInstanceState);
 		ArrayAdapter<String> menuAdapter = new ArrayAdapter<String>(
-				getActivity(), R.layout.menu_list_textview, android.R.id.text1,
+				getActivity(), R.layout.menu_list_textview, R.id.menu_list_text_view,
 				mMenus);
 		setListAdapter(menuAdapter);
 	}
@@ -45,6 +55,9 @@ public class MenuFragment extends ListFragment {
 	public void onListItemClick(ListView l, View v, int position, long id) {
 		super.onListItemClick(l, v, position, id);
 
+		position -= l.getHeaderViewsCount();
+		if (position < 0) // when click on header view
+			return;
 		String selectedMenu = mMenus[position];
 		ProductDisplay pd = null;
 		if (getActivity() instanceof ProductDisplay) {
